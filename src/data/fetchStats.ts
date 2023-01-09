c6d0ac97ff28429cf39e261ecca0ae22eef1e3aa
Ian Gregory <justcheesy@outlook.com>
2023-01-08 22:21:34 -0500

    Round cask apps count a bit

--- src/data/fetchStats.ts
+++ src/data/fetchStats.ts
@@ -1,4 +1,4 @@
-import { writeFile } from 'fs/promises';
+import { writeFile } from "fs/promises";
 
 fetchStatsOrDoNothing();
 
@@ -10,20 +10,29 @@ export async function fetchStatsOrDoNothing() {
 }
 
 async function storeStats(stats: Awaited<ReturnType<typeof fetchStats>>) {
-  return writeFile('./src/data/stats.json', prettyJSONStringify(stats) + "\n");
+  return writeFile("./src/data/stats.json", prettyJSONStringify(stats) + "\n");
 }
 
 async function fetchStats() {
-  const data = await fetch('https://formulae.brew.sh/api/versions-casks.json');
+  const data = await fetch("https://formulae.brew.sh/api/versions-casks.json");
   const jsonData = await data.json();
 
-  const appsCount = Object.keys(jsonData).length;
+  const brewCaskAppsCount = Object.keys(jsonData).length;
 
   return {
-    appsCount,
+    brewCaskAppsCount,
   };
 }
 
 function prettyJSONStringify(value: any) {
   return JSON.stringify(value, undefined, 2);
+}
+
+/**
+ * Rounds `value` down (toward zero) to the nearest multiple of `increment`.
+ *
+ * e.g., `floorToIncrement(7, 2) === 6`
+ */
+export function floorToIncrement(value: number, increment: number) {
+  return Math.floor(value / increment) * increment;
 }
